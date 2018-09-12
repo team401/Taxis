f97b36086a0159c43485281c170bab6eb52558bf
Cameron <camearle20@users.noreply.github.com>
2018-09-12 13:16:21 -0400

    Added auto mode to tune scrub radius

--- src/main/kotlin/org/team401/taxis/diffdrive/autotune/TuneTrackScrubFactor.kt
+++ src/main/kotlin/org/team401/taxis/diffdrive/autotune/TuneTrackScrubFactor.kt
@@ -1,25 +1,45 @@
 package org.team401.taxis.diffdrive.autotune
 
+import com.ctre.phoenix.motorcontrol.ControlMode
 import org.snakeskin.auto.steps.AutoStep
 import org.snakeskin.component.TankDrivetrain
 import org.snakeskin.units.AngularDistanceUnit
+import org.snakeskin.units.LinearDistanceUnit
+import org.snakeskin.units.measure.distance.angular.AngularDistanceMeasureCTREMagEncoder
+import org.snakeskin.units.measure.distance.angular.AngularDistanceMeasureDegrees
 
 /**
  * @author Cameron Earle
  * @version 9/10/18
  *
  * Tunes the track scrub factor.  Make sure no other threads are updating the drive
+ *
+ * Right turns increase angle
  */
-class TuneTrackScrubFactor(val drivetrain: TankDrivetrain, turns: Int = 10, direction: Double = 1.0): AutoStep() {
-    private val turnDegrees = turns * 360.0
+class TuneTrackScrubFactor(val drivetrain: TankDrivetrain, turns: Int = 10, power: Double = 1.0, direction: Boolean = true): AutoStep() {
+    private val turnRadians = turns * 2 * Math.PI
+    private val turnPower = if (direction) power else -power
 
     override fun entry(currentTime: Double) {
+        drivetrain.setYaw(AngularDistanceMeasureDegrees(0.0))
+        drivetrain.left.setPosition(AngularDistanceMeasureCTREMagEncoder(0.0))
+        drivetrain.right.setPosition(AngularDistanceMeasureCTREMagEncoder(0.0))
     }
 
     override fun action(currentTime: Double, lastTime: Double) {
-        val yaw = drivetrain.getYaw().toUnit(AngularDistanceUnit.Standard.DEGREES).value
-        if (yaw >= turnDegrees) {
+        drivetrain.tank(ControlMode.PercentOutput, turnPower, -turnPower)
+        val yaw = Math.abs(drivetrain.getYaw().toUnit(AngularDistanceUnit.Standard.RADIANS).value)
+        if (yaw >= turnRadians) {
+            val leftPos = drivetrain.left.getPosition().toLinearDistance(drivetrain.wheelRadius).toUnit(LinearDistanceUnit.Standard.INCHES).value
+            val rightPos = drivetrain.right.getPosition().toLinearDistance(drivetrain.wheelRadius).toUnit(LinearDistanceUnit.Standard.INCHES).value
+            val arcLength = (leftPos + rightPos) / 2
 
+            val trackWidth = 2 * yaw / arcLength
+
+            println("Empirical Trackwidth (Inches): $trackWidth")
+            println("Track Scrub Factor: ${drivetrain.wheelbase.toUnit(LinearDistanceUnit.Standard.INCHES).value / trackWidth}")
+
+            done = true
         }
     }
 
