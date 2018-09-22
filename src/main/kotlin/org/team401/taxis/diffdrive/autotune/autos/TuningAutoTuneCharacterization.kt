41def4db9a46d6508989934d8e693e36c84e8bff
Cameron <camearle20@users.noreply.github.com>
2018-09-22 14:22:52 -0400

    Added delay in characterization

--- src/main/kotlin/org/team401/taxis/diffdrive/autotune/autos/TuningAutoTuneCharacterization.kt
+++ src/main/kotlin/org/team401/taxis/diffdrive/autotune/autos/TuningAutoTuneCharacterization.kt
@@ -1,9 +1,11 @@
 package org.team401.taxis.diffdrive.autotune.autos
 
 import org.snakeskin.auto.RobotAuto
+import org.snakeskin.auto.steps.DelayStep
 import org.snakeskin.auto.steps.LambdaStep
 import org.snakeskin.auto.steps.SequentialSteps
 import org.snakeskin.component.TankDrivetrain
+import org.snakeskin.units.Seconds
 import org.snakeskin.units.measure.time.TimeMeasure
 import org.team401.taxis.diffdrive.autotune.CollectLinearAccelerationData
 import org.team401.taxis.diffdrive.autotune.CollectLinearStictionData
@@ -24,6 +26,7 @@ class TuningAutoTuneCharacterization(val drivetrain: TankDrivetrain,
         val collectAccel = CollectLinearAccelerationData(drivetrain, accelPower, accelRuntime)
         return SequentialSteps(
                 collectStiction,
+                DelayStep(20.Seconds),
                 collectAccel,
                 LambdaStep {
                     val characterization = DriveCharacterization.characterizeDrive(collectStiction.data, collectAccel.data as ArrayList<DriveCharacterization.AccelerationDataPoint>)
